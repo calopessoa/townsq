bcdb27d4cfbdbce8528e532157908dec7a74dd60
Carlos <nashlasterby@gmail.com>
2022-11-01 14:15:17 -0300

    Initial Commit: cypress setup

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,6 @@
+/* eslint-disable no-undef */
+describe('Townsq front-page tests', () => {
+  it('should correctly visit and render the main page', () => {
+    cy.visit('https://townsq.com.br/')
+  });
+});
