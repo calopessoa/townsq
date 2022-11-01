5c7754c61c5ff603d14c09b8369748864cc91c86
Carlos <nashlasterby@gmail.com>
2022-11-01 15:06:12 -0300

    feat: tests townsq front page

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,17 @@
 /* eslint-disable no-undef */
 describe('Townsq front-page tests', () => {
   it('should correctly visit and render the main page', () => {
+    // cy.visit('https://www.uol.com.br/')
     cy.visit('https://townsq.com.br/')
   });
+
+  it('should click on `Conheça o App`', () => {
+    // cy.visit('https://www.uol.com.br/')
+    cy.xpath('//*[@id="post-5017"]/div/div/div/div/section[1]/div/div/div[1]/div/div/div[3]/div/div/a/span/span').click();
+  });
+
+  it('should click on `Materiais para Download` and redirect to a page with its contents', () => {
+    cy.xpath('//*[@id="page"]/div[2]/div/section[1]/div/div/div[2]/div/div/div[2]/div/div/p/a[3]').click();
+  })
+
 });
