63bc9fe150e1bc1df22ab63c0a22518bfb6a8cf7
Carlos <nashlasterby@gmail.com>
2022-11-01 15:22:19 -0300

    feat: filling up the form

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -10,8 +10,32 @@ describe('Townsq front-page tests', () => {
     cy.xpath('//*[@id="post-5017"]/div/div/div/div/section[1]/div/div/div[1]/div/div/div[3]/div/div/a/span/span').click();
   });
 
-  it('should click on `Materiais para Download` and redirect to a page with its contents', () => {
-    cy.xpath('//*[@id="page"]/div[2]/div/section[1]/div/div/div[2]/div/div/div[2]/div/div/p/a[3]').click();
-  })
+  // it('should click on `Materiais para Download` and redirect to a page with its contents', () => {
+  //   cy.xpath('//*[@id="page"]/div[2]/div/section[1]/div/div/div[2]/div/div/div[2]/div/div/p/a[3]').click();
+  // });
+});
 
+describe('Test functionality to request a budget', () => {
+  it('should visit /gestao-condominios', () => {
+    cy.visit('https://townsq.com.br/gestao-condominios/')
+  });
+
+  it('should click on `Ver Planos`', () => {
+    // cy.visit('https://www.uol.com.br/')
+    cy.xpath('//*[@id="post-1912"]/div/div/div/div/section[12]/div/div/div/div/div/div/div/div/a/span/span').click();
+  });
+
+  it('should click on `Saiba Mais` or `Fale com um Consultor`', () => {
+    cy.xpath('//*[@id="post-2282"]/div/div/div/div/section[3]/div/div/div[1]/div/div/section/div/div/div/div/div/div/div/div/a/span/span').click();
+  });
+
+  it('tests if it`s possible to fill the budget-form', () => {
+    cy.xpath('//*[@id="rd-text_field-qxZCGuVDJeZc1Z50Ql9sMA"]').type('Cliente T ownsq');
+    cy.xpath('//*[@id="rd-select_field-AnRsrlMh_CKJccK-DJ9Izw"]').select('Quero ser síndico');
+    cy.xpath('//*[@id="rd-email_field-Gyqg33gCvIhRtkCRYwxdSw"]').type('clientetown@email.com');
+    cy.xpath('//*[@id="rd-phone_field-Os3LgjND9xnwPJiBCBSdvQ"]').type('83988880000');
+    cy.xpath('//*[@id="rd-select_field-oe7nFgKtOERC5WT1p88ldA"]').select('De 50 a 100 unidades');
+    cy.xpath('//*[@id="rd-select_field-QNTPdeUA8Ra4XtuGeGBM_Q"]').select('Nenhum');
+    cy.xpath('//*[@id="rd-select_field-sYDHH89deG7MhjstflkKNw"]').select('TownSq Essencial');
+  });
 });
